84bab77bcfcc78836e143482f122e4d9cad1174f
ugboaja-uchechi <stephanieuchechi754@gmail.com>
2023-09-29 01:02:36 +0100

    Adjust code to display the right data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React, { useState } from 'react';
-import tournament from './Data/data.json'
+import tournament from './Data/data.json';
 
 const App = () => {
   const [round, setRound] = useState(1);
   const [data, setData] = useState(tournament);
 
-  const challengers = data.filter(item => item.round === round).map(item => item.challenger);
-  const challenged = data.filter(item => item.round === round).map(item => item.challenged);
-  const winners = data.filter(item => item.round === round).map(item => item.winner);
+  const roundData = data.filter(item => item.round === round);
 
   const nextRound = () => {
-    // Check if there is a next round in your data
+    // Checks if there is a next round
     const maxRound = Math.max(...data.map(item => item.round));
     if (round < maxRound) {
       setRound(round + 1);
@@ -21,30 +19,25 @@ const App = () => {
     <div>
       <h1>Round {round}</h1>
       <div>
-        <h2>Challengers</h2>
+        <h2>Challenger and Challenged</h2>
         <ul>
-          {challengers.map((challenger, index) => (
-            <li key={index}>{challenger ? `${challenger.firstname} ${challenger.lastname}` : 'No challenger'}
+          {roundData.map((item, index) => (
+            <li key={index}>
+              {item.challenger ? `1 ${item.challenger.firstname}  ${item.challenger.lastname}` : 'No challenger'}
+              &nbsp;vs&nbsp;
+              {item.challenged ? `2 ${item.challenged.firstname} ${item.challenged.lastname}` : 'Not challenged'}
             </li>
           ))}
         </ul>
       </div>
-      <div>
-        <h2>Challenged</h2>
-        <ul>
-          {challenged.map((challenged, index) => (
-            <li key={index}>{challenged ? `${challenged.firstname} ${challenged.lastname}` : 'Not challenged'}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
+      {/* <div>
         <h2>Winners</h2>
         <ul>
-          {winners.map((winner, index) => (
-            <li key={index}>{winner ? winner.firstname : 'No winner'}</li>
+          {roundData.map((item, index) => (
+            <li key={index}>{item.winner ? item.winner.firstname : 'No winner'}</li>
           ))}
         </ul>
-      </div>
+      </div> */}
       <button onClick={nextRound}>Next Round</button>
     </div>
   );
